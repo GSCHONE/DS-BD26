912a8f5d49eedffb859479ee32cad87957fee28d
GSCHONE <118915762+GSCHONE@users.noreply.github.com>
2022-12-10 02:31:08 +0100

    Last_one
    Début d'interrogation API IMDB
    Treès puissant mais va falloir payer $$

--- IMPORT_DATA.R
+++ IMPORT_DATA.R
@@ -3,60 +3,78 @@
   +  +#PROJECT SOURCES
 library(readxl)
 library(tidyverse)
-
-#1 DONNEES PUBLIQUES, MIS 12/06/2022 VARIABLE Y : https://www.data.gouv.fr/fr/datasets/films-ayant-realise-plus-dun-million-dentrees/
-colName=c("titre",
-      "nationalité",
+library(lubridate)
+  library(json)
+  library(httr)
+library(rjson)
+  #1 DONNEES PUBLIQUES, MIS 12/06/2022 VARIABLE Y : https://www.data.gouv.fr/fr/datasets/films-ayant-realise-plus-dun-million-dentrees/
+COL_NAME=c("titre",
+      "nationalit?",
       "sortie",
-      "entrées (millions)",
+      "entr?es (millions)",
       "rang")
-colType=c("numeric","text","text","date","numeric")
+COL_TYPE=c("numeric","text","text","date","numeric")
 
-data<-data.frame(matrix(nrow=0,ncol=length(colName)))
-colnames(data)<-colName
+DATA<-data.frame(matrix(nrow=0,ncol=length(COL_NAME)))
+colnames(DATA)<-COL_NAME
 
 
 for (k in 2003:2021){
-  ENTREES<-read_excel("D:/DATA_SCIENCE/PROJET/DATA/1_ENTREES.xlsx",
+  ENTREES<-read_excel("./DATA/1_ENTREES.xlsx",
                       skip=6,
-                      sheet=as.character(k),
-                      col_types=colType)
-  names(ENTREES)[1]="rang"
+                      sheet=as.character(k), #k dÃ©finit l'onlet du fichier EXCEL
+                      col_types=COL_TYPE)
+  names(ENTREES)[1]="rang" ###DEFAU DE NOMMAGE DES PREMIERES COLONNES. RENOMMER SYSTEMATIQUEMENT PERMET D'EVITER LES ERREURS
  
- data=rbind(data,ENTREES)
- k=k+1C sur les films à plus de 1 millions depuis 2003 -> 876 films
-MAJ fiche START 
-Script R d'importation
+  DATA=rbind(DATA,ENTREES) ### ON AJOUTE ONGLET PAR ONGLET LES DONNEES
+ k=k+1
+
 }
 
 
 
-data %>%
+DATA %>%
   select(-rang) %>% 
-  rename(Y=`entrées (millions)`) %>% 
-  arrange(desc(Y))->data
+  rename(Y=`entrÃ©es (millions)`) %>%
+  rename(NAT=nationalitÃ©) %>% 
+  arrange(desc(Y))->DATA
 
-
+as.Date(as.POSIXct("2013-01-01 07:00", 'GMT'))
 
 ###ATTENTION ECARTS AVEC LES VRAIS DATA --> ON VA ALLER REQUETER LES NOMS DES FILMS LISTES 
 
 
 BASE_NOM <- unique(data$titre)
-data %>% group_by(titre) %>% summarise(totY=sum(Y)) %>% arrange(desc(totY)) %>% 
-  rename(TITRE=titre)->TOT_TITRE
-data %>% select(titre,nationalité,sortie)-> REF_TITRE
-REF_TITRE %>% rename(NAT=nationalité,TITRE=titre,SORTIE=sortie)->REF_TITRE
+DATA %>% filter(!is.na(titre))->DATA
+
+DATA %>% 
+     group_by(titre,sortie) %>%
+     summarise(Y=sum(Y),.groups = 'drop') %>%
+     rename(TITRE=titre)->TOT_TITRE
+
+#WARNING PROBLEME DE DOUBLON DANS LES TITRES 
 
 
-REF_TITRE %>% left_join(TOT_TITRE)-> DATA_OK
-unique(DATA_OK$NAT) #852 noms différents
+TOT_TITRE %>% group_by(TITRE) %>% summarise(n=n()) %>% filter(n>1) %>% arrange(desc(n))->DOUBLON
+TOT_TITRE %>% filter(TITRE %in% DOUBLON$TITRE)
+data %>% filter(titre %in% DOUBLON$TITRE)
 
+### 2 FILMS EN DOUBLONS, COCO DESSIN ANIMEE ET CELUI AVEC GAD ELMALEH NON ?  // 4 FANTASTIQUES IDEM , DEUX FILMS 2015 ET 2005
 
-unique(DATA_OK$TITRE)
+write.csv(TOT_TITRE,"./DATA_CLEAN/FILMS_1MILLIONS.csv")
 
-DATA_OK %>% filter(is.na(TITRE))
-summary(TOT_TITRE$totY)
-dim(TOT_TITRE)
-DATA_OK %>% filter(is.na(totY))
-data %>% filter(is.na(Y))
-DATA_
+TOT_TITRE %>% 
+     mutate(ifelse(TI))
+
+for (k in 1:nrow(TOT_TITRE)){
+     nom=paste(TOT_TITRE[k,"TITRE"],year(as.Date(TOT_TITRE[k,"sortie"]$sortie)))
+     print(nom)
+     URL=paste0("https://imdb-api.com/en/API/SearchMovie/k_04ykrtva/",nom)
+     result=GET(URL)
+     result_jon=fromJSON(rawToChar(result$content))
+     if(is.null(dim(result_jon$results))){
+          TOT_TITRE$id="INCONNU"
+     }else{
+          TOT_TITRE$id=result_jon$results[[1]]$id
+     }
+}
