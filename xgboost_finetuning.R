cf122518ef8525cb1937d02c44bd6c1ec2b7b30c
gschone <gilles.schone@crtc.ccomptes.fr>
2023-03-04 09:04:53 +0000

    v0

--- xgboost_finetuning.R
+++ xgboost_finetuning.R
@@ -17,6 +17,17 @@ rec_basic <-
 prep(rec_basic)
 juice(prep(rec_basic))->data_receipe
 
+rec_test <- 
+     recipe(Y~., data = data_reg_test) %>% 
+     
+     step_normalize(all_numeric_predictors()) %>% 
+     
+     step_impute_mean(all_numeric_predictors()) %>%
+     step_impute_mode(all_nominal_predictors()) %>%
+     step_zv(all_nominal_predictors()) %>% 
+     step_dummy(all_nominal_predictors())
+prep(rec_test)
+juice(prep(rec_test))->data_test
 don=data_receipe
 set.seed(1234)
 data_reg_split <- initial_split(data_classif, prop = 0.8, strata = Y) # par defaut prop = 0.75
@@ -31,16 +42,16 @@ YY <- data_reg_train$Y
 
 ##XGBOOST PARAM 
 
-data_reg_resample <- vfold_cv(data_reg_train, v = 5)
+data_reg_resample <- vfold_cv(data_reg_train, v = 3)
 xgboost_tune <- 
      parsnip::boost_tree(
           mode = "classification",
-          trees = 1000,
+          trees = 15,
           min_n = tune(),
           tree_depth = tune(),
           learn_rate = tune(),
-          loss_reduction = tune()
-     ) %>%
+          loss_reduction = tune(),
+          num_class = 3) %>%
      set_engine("xgboost", objective = "multi:softmax")
 # grid specification
 xgboost_params <- 
@@ -63,10 +74,27 @@ xgboost_wf <-
      add_model(xgboost_tune) %>% 
      add_recipe(rec_basic)
 
-xgboost_tuned <- tune::tune_grid(
+xgboost_tuned2 <- tune::tune_grid(
      object = xgboost_wf,
      resamples = data_reg_resample,
      grid = xgboost_grid,
-     metrics = yardstick::metric_set(accuracy,roc_auc),
+     metrics = yardstick::metric_set(accuracy,kap),
      control = tune::control_grid(verbose = TRUE)
 )
+xg_best_params <- xgboost_tuned %>%
+     tune::select_best("accuracy")
+
+summary(xgboost_tuned)
+xgboost_tuned
+show_best(xgboost_tuned)
+autoplot(xgboost_tuned,metric="accuracy")
+xg_stage_2_model <- xgboost_tune %>% 
+     finalize_model(parameters = xg_best_params) %>% 
+     fit(Y~.,juice(prep(rec_basic)))
+data_train=prep(rec_basic)
+Yhat=predict(xg_stage_2_model,data_test)
+summary(Yhat)
+RES=data.frame(Y=data_reg_test$Y,Yhat=predict(xg_stage_2_model,data_test))
+summary(xg_stage_2_model)
+write_rds(xgboost_tuned,"xgParam_1000T_50grid.rds")
+#15h44
